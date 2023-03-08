2f4d547e4f78df4067c9fe6b28c99703d97a971b
feruzbekismoilov <ismoilovferuz0858@gmail.com>
2023-03-08 14:09:00 +0500

    add btn in item

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 
 
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+const coundownEl = document.getElementById('coundown');
+setInterval(updateCountdown, 1000);
+function updateCountdown() {
+    const minutes = Math.floor(time/60);
+    const seconds = time % 60;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+    coundownEl.textContent= `${minutes}:${seconds}`;
+    time--;
+}
